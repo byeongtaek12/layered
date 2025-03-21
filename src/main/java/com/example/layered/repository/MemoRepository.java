15bbbe6ba8489ff48253ddb356826efb906eef74
obt <byeongtaek12@naver.com>
2025-03-21 15:08:43 +0900

    style: MemoRepository클래스 )불필요한 import 제거
    ``

--- src/main/java/com/example/layered/repository/MemoRepository.java
+++ src/main/java/com/example/layered/repository/MemoRepository.java
@@ -1,9 +1,9 @@
 package com.example.layered.repository;
 
-import com.example.layered.dto.MemoRequestDto;
+
 import com.example.layered.dto.MemoResponseDto;
 import com.example.layered.entity.Memo;
-import com.example.layered.service.MemoService;
+
 
 import java.util.List;
 
